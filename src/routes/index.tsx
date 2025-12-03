916ceb6086c7701266950086eb44dbf246ea3419
xyfll7 <l.7@qq.com>
2025-12-03 15:25:15 +0800

    Adds Radix UI and a test button
    Adds Radix UI react-slot and a test button component.
    
    This allows for accessible and unstyled components to be used.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import logo from "../logo.svg";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
 	component: App,
@@ -8,31 +8,8 @@ export const Route = createFileRoute("/")({
 function App() {
 	return (
 		<div className="text-center">
-			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<img
-					src={logo}
-					className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
-				</p>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://tanstack.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn TanStack
-				</a>
+			<header >
+				<Button size={"sm"}>Test</Button>
 			</header>
 		</div>
 	);
