b46b6660b31c05a05b27b7a1d5827cb97faa6cdc
xyfll7 <l.7@qq.com>
2025-12-13 20:22:14 +0800

    Remove debug print statements from shortcut handling loop

--- src-tauri/src/my_test.rs
+++ src-tauri/src/my_test.rs
@@ -14,17 +14,11 @@ pub fn set_shortcuts(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> 
         loop {
             let keys = device_state.get_keys();
             let is_pressed = keys.contains(&Keycode::RControl);
-
             if is_pressed && !was_pressed {
-                println!("✅ ControlRight KeyPress");
-                // 执行按下逻辑
                 my_windows::window_input_method_editor_show(&app_clone);
             } else if !is_pressed && was_pressed {
-                println!("✅ ControlRight KeyRelease");
                 my_windows::window_input_method_editor_hide(&app_clone);
-                // 执行释放逻辑
             }
-
             was_pressed = is_pressed;
             thread::sleep(Duration::from_millis(10)); // 轮询间隔
         }
